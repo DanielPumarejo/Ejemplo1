cbcb91b336ad2139dae7bfd49471bfec3e5f6efa
danielpumarejo <daniellfpd@hotmail.com>
2019-07-18 15:10:34 -0500

    primer commit

---
+++ Nomina/DAL/BaseDatos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAL
+{
+    public class BaseDatos
+    {
+        string cadenaconexion;
+        SqlConnection conexion;
+
+        public BaseDatos()
+        {
+            cadenaconexion = "Server=myServerName\myInstanceName;Database=myDataBase;User Id=sa;Password = 123456; ";
+            conexion = new SqlConnection(cadenaconexion);
+        }
+
+        public string abrir()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+            {
+                cerrar();
+                conexion.Open();
+                return "abierta";
+            }
+            return conexion.Open();
+            
+
+        }
+
+        public string cerrar()
+        {
+            conexion.Close();
+            return "cerrada";
+        }
+    }
+}
